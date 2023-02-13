53fdd8ffed8df2cd8a88d3e54bace0dcc4ffa876
Abhineet-08 <113853385+Abhineet-08@users.noreply.github.com>
2023-02-14 00:49:05 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -6,21 +6,8 @@ import Header from './components/Header';
 
 const App = () => {
 	const [notes, setNotes] = useState([
-		{
-			id: nanoid(),
-			text: 'This is my first note!',
-			date: '15/04/2021',
-		},
-		{
-			id: nanoid(),
-			text: 'This is my second note!',
-			date: '21/04/2021',
-		},
-		{
-			id: nanoid(),
-			text: 'This is my third note!',
-			date: '28/04/2021',
-		},
+		
+		
 		{
 			id: nanoid(),
 			text: 'This is my new note!',
